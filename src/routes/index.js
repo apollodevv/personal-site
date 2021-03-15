23f45c80e25ced9cc80d0925676748d921d6d170
apollo <80532139+apollodevv@users.noreply.github.com>
2021-03-15 17:03:08 +0100

    api route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,15 @@ const express = require("express")
 const app = express();
 app.set('view engine', 'ejs');
 
+
+app.get('/', function(req, res) {
+  res.sendFile(dirname__ + '/src/public/index.ejs);
+});
+
+app.get('/api', function(req, res) {
+  res.sendFile(dirname__ + '/src/utils/api/index.ejs);
+});
+
 app.listen(3000, function (err) {
     if (err) return console.log(err)
     console.log('200 OK')
