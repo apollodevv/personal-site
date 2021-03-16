23fb5031f09ae7162ff04efc5770476e2cb53ba4
apollo <80532139+apollodevv@users.noreply.github.com>
2021-03-16 17:47:42 +0000

    fix routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,8 @@
-const express = require("express")
+const express = require("express");
+const router = express.Router();
 
-const app = express();
-app.set('view engine', 'ejs');
-
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/src/public/index.ejs');
-});
-
-app.get('/api', function(req, res) {
-  res.sendFile(__dirname + '/src/utils/api/index.ejs');
-});
-
-app.listen(3000, function (err) {
-    if (err) return console.log(err)
-    console.log('200 OK')
+router.get('/', function(req, res) {
+    res.render("index");
 })
+
+module.exports = router;
